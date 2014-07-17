c445679e903155d02d416a929c091a6662a3b03f
Piotr Fryga <piotr.fryga@allegro.pl>
2014-07-17 15:43:47 +0200

    example view

--- test/app/scripts/main.js
+++ test/app/scripts/main.js
@@ -12,7 +12,19 @@ require.config({
 });
 
 require([
-    'backbone'
-], function (Backbone) {
-    Backbone.history.start();
+    'backbone',
+    'jquery'
+], function (Backbone, $) {
+	// console.log(1);
+    var ListView = Backbone.View.extend({
+    	el: $('body'),
+    	initialize: function() {
+    		this.render();
+    	},
+    	render: function () {
+    		$(this.el).append("hello world!");
+    	}
+    });
+
+    var listView = new ListView();
 });

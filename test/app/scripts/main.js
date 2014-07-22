2e7d31912d264c3103a0d90bd6f0ffab18c346e2
Piotr Fryga <piotr.fryga@allegro.pl>
2014-07-22 12:13:04 +0200

    ex

--- test/app/scripts/main.js
+++ test/app/scripts/main.js
@@ -15,16 +15,50 @@ require([
     'backbone',
     'jquery'
 ], function (Backbone, $) {
-	// console.log(1);
+
+    var Item = Backbone.Model.extend({
+        defaults: {
+            part1: 'hello',
+            part2: 'world'
+        }
+    });
+
+    var List = Backbone.Collection.extend({
+        model: Item
+    });
+
     var ListView = Backbone.View.extend({
     	el: $('body'),
+        events: {
+            'click button#add': 'addItem'
+        },
     	initialize: function() {
+            this.counter = 0;
     		this.render();
     	},
     	render: function () {
-    		$(this.el).append("hello world!");
-    	}
+            $(this.el).append("<button id='add'>Add list item</button>");
+    		$(this.el).append("<ul id=\"items\"></ul>");
+    	},
+        addItem: function() {
+            this.counter++;
+            $('ul#items', this.el).append("<li>hello world " + this.counter + "</li>");
+        },
+        appendItem: function(item){
+            $('ul#items', this.el).append("<li>" + item.get('part1') + " " + item.get('part2') + "</li>");
+        }
     });
 
+    // var ob = {
+    //     super: $('#test'),
+    //     makeSth: function() {
+    //         $('#test').html('append!');
+    //         this.super.html('append2');
+    //         console.log(this.super.html());
+    //     }
+    // };
+
+    // ob.makeSth();
+    
     var listView = new ListView();
 });
